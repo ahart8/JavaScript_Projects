385afa7d1eb774d71da4c297443121e1db861a2a
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-02-19 20:49:05 -0500

    Final Checkin

--- Basic JavaScript Projects/Project4_dictionaries/js/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/js/main.js
@@ -1,17 +1,17 @@
 
 
 //Create a dictionary and set an HTML element to a value in the dictionary
-function myFirstDictionary(){ 
-    var RockBands={
-        Name:"The Beatles",
-        Album:"Abby Road",
-        RythmGuitar:"John Lennon",
-        BassGuitar:"Paul McCartney",
-        LeadGuitar:"George Harrison",
-        Drums:"Ringo Star"
-        
+function myFirstDictionary() {
+    var RockBands = {
+        Name: "The Beatles",
+        Album: "Abby Road",
+        RythmGuitar: "John Lennon",
+        BassGuitar: "Paul McCartney",
+        LeadGuitar: "George Harrison",
+        Drums: "Ringo Star"
+
 
     };
     delete RockBands.Album; //Remove the key before it id displayed
-    document.getElementById("Dictionary").innerHTML=RockBands.Album;
+    document.getElementById("Dictionary").innerHTML = RockBands.Album;
 }
