29eeb787264537b90e133d2737248e3e46bf5654
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-03-02 00:45:19 -0500

    I am checking in code for project 10

---
+++ Basic JavaScript Projects/Project10_loops_arrays/js/main.js
@@ -0,0 +1,68 @@
+//THIS FUNCTION USES A WHILE LOOP
+function  callLoop(){
+var digit="";
+let x=1;
+
+while(x<10){
+  
+  digit += "<br>" +x;
+  x++;
+}
+document.getElementById("loop").innerHTML=digit;
+}
+//END FUNCTION
+
+
+ //THIS FUNCTION USES A FOR LOOP 
+  var bands=["Smashing Pumpkins","Stone Temple Pilots","Sound Garden","Red Hot Chili Peppers","Alice In Chains"];
+  var list ="";
+  var counter;
+ 
+  function forLoop(){
+  for (counter=0;counter<bands.length;counter++){
+    list +=bands[counter]+"<br>";
+  }
+    document.getElementById("myList").innerHTML=list;
+}
+//END FUNCTION
+
+//THIS FUNCTION USES AN ARRAY
+function arrayFunction(){
+var floridaNativePlants=[];
+
+floridaNativePlants[0] = "American Beautyberry";
+floridaNativePlants[1] = "Firebush";
+floridaNativePlants[2] = "Coral Honeysuckle";
+floridaNativePlants[3] = "Carloina Jasmine";
+floridaNativePlants[4] = "Passion Flower";
+floridaNativePlants[5] = "Swamp Lilly";
+
+
+document.getElementById("myArray").innerHTML=floridaNativePlants[3];
+}
+//END
+
+
+function returnFunction(){
+   document.getElementById("myReturn").innerHTML="Amy Hart";
+}
+  let user={
+  
+   firstName:"Amy",
+    lastName:"Hart",
+   username:"ahart",
+   pwd:"test",
+ 
+   getFullName:function(){
+
+    return this.firstName +" "+this.lastName
+    }
+
+  };
+ console.log(user);
+ console.log(user.getFullName());
+ 
+
+
+
+
